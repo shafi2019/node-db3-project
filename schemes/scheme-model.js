bc1c4236d384cc5f78caa6903098b3d38fd3c97d
Shafi Masoumi <masoumi2017@icloud.com>
2020-02-06 15:37:07 -0800

    FindSteps and add done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -6,11 +6,32 @@ function find() {
 
 function findById(id) {
     return db('schemes')
-    .where({ id })
-    .first();
+        .where({ id })
+        .first();
 }
+
+function findSteps(id) {
+    return db('steps')
+        .select(
+            'steps.id',
+            'schemes.scheme_name',
+            'steps.step_number',
+            'steps.instructions'
+        )
+        .join('schemes', 'schemes.id', 'steps.scheme_id')
+        .where({ scheme_id: id });
+}
+
+function add(newData) {
+    return db('schemes')
+        .insert(newData)
+        .then(id => findById(id[0]));
+};
+
 
 module.exports = {
     find,
-    findById
-}
+    findById,
+    add,
+    findSteps
+};
